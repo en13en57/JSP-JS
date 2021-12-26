311d394e2fdc691ddd53e3f32cc44dd79a43d10f
Doo O <en13en57@gmail.com>
2021-12-26 12:36:26 +0900

    programmers

---
+++ Programmers/src/main/java/kr/green/Test/Programmers/kjgh.java
@@ -0,0 +1,11 @@
+package kr.green.Test.Programmers;
+
+public class kjgh {
+	public static void main(String[] args) {
+		String str = "ABCDEFG"; 
+		String result = str.substring(1, 6);
+		System.out.println(result);
+		 //결과값EFG
+	}
+	
+}
